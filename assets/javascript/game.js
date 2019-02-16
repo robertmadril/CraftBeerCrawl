7b700fc170b31d8292836afdfb6c50dee7f56921
robertmadril <robertmmadril@gmail.com>
2019-02-16 08:21:21 -0700

    add jquery logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,20 +35,41 @@ var drink = {
 
 };
 
+var chosenDrinker = ""
+
 function drinkAction(c, b) {
     c.tolerance = c.tolerance - b.abv;
     c.drinkingAbility = c.drinkingAbility + c.drinkingAbility;
     b.ounces = b.ounces - c.drinkingAbility;
 }
 
+$("#malt-pick").on("click", function() {
+    chosenDrinker = "#malt-pick";
+    $("#pick-drinker").empty();
+    $("#drinker").append("<div>MALT PICK</div>")
+});
+
+$("#gose-pick").on("click", function() {
+    chosenDrinker = "#gose-pick";
+    $("#pick-drinker").empty();
+    $("#drinker").append("<div>GOSE PICK</div>")
+});
+
+$("#barrel-pick").on("click", function() {
+    chosenDrinker = "#barrel-pick";
+    $("#pick-drinker").empty();
+    $("#drinker").append("<div>BARREL PICK</div>");
+
+});
+
+$("#drink-btn").on("click", function() {
+    drinkAction();
+})
 
 
 /*
 
 Create a game flow that:
-
-Starts with all three characters displayed in pick drinker and random tolerance/drinking ability assigned at start of game
-Player chooses a character
 That character is moved into the drinker section, beers populate with random ABV and "ounces"
 Player picks a beer and it is moved into the bar
 
@@ -58,9 +79,6 @@ call drinkAction function
 
 
 */
-
-$("#bar").on("click", function() {
-    drinkAction();
 //If last enemy ounces <= 0, game is over, win
 //If tolerance <= 0, game is over, lost
 })
