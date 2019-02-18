f3eb0a6ca274eeacf74ac6914a02663492f6de51
robertmadril <robertmmadril@gmail.com>
2019-02-17 19:20:53 -0700

    add js button roundand attribute to hold values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,20 @@
+//object elements containing three characters and three 'enemy' beers
+
 var drink = {
     malt: {
         name: "The Malt Master",
-        tolerance: Math.floor(Math.random() * 101),
-        drinkingAbility: Math.floor(Math.random() * 10),
+        tolerance: Math.floor(Math.random() * 75) + 50,
+        drinkingAbility: Math.floor(Math.random() * 10) + 3,
     },
     gose: {
         name: "The Gose God",
-        tolerance: Math.floor(Math.random() * 101),
-        drinkingAbility: Math.floor(Math.random() * 15),
+        tolerance: Math.floor(Math.random() * 75) + 50,
+        drinkingAbility: Math.floor(Math.random() * 10) + 3,
     },
     barrel: {
         name: "The Barrel Baron",
-        tolerance: Math.floor(Math.random() * 101),
-        drinkingAbility: Math.floor(Math.random() * 15),
+        tolerance: Math.floor(Math.random() * 75) + 50,
+        drinkingAbility: Math.floor(Math.random() * 10) + 3,
     },
 
 };
@@ -21,42 +23,69 @@ var bev = {
 
     ipa: {
         name: "IPA",
-        abv: Math.floor(Math.random() * 15),
-        ounces: Math.floor(Math.random() * 40),
+        abv: Math.floor(Math.random() * 13) + 7,
+        ounces: Math.floor(Math.random() * 48) + 16,
     },
 
     stout: {
         name: "Stout",
-        abv: Math.floor(Math.random() * 15),
-        ounces: Math.floor(Math.random() * 40),
+        abv: Math.floor(Math.random() * 16) + 10,
+        ounces: Math.floor(Math.random() * 48) + 16,
     },
 
     pils: {
         name: "pilsner",
-        abv: Math.floor(Math.random() * 101),
-        ounces: Math.floor(Math.random() * 101)
+        abv: Math.floor(Math.random() * 9) + 4,
+        ounces: Math.floor(Math.random() * 48) + 16,
     },
 
 };
 
+//global variables
 var chosenDrinker;
 var currBeer;
-var playPick = false;
-var beerPick = false;
+var attackTol;
+var attackDA;
+var attackDAInit;
+var defendABV;
+var defendOZ;
 
-$("#malt-tol").text("Tolerance: " + drink.malt.tolerance);
-$("#malt-da").text("Drinking Ability: " + drink.malt.drinkingAbility);
-$("#gose-tol").text("Tolerance: " + drink.gose.tolerance);
-$("#gose-da").text("Drinking Ability: " + drink.gose.drinkingAbility);
-$("#barrel-tol").text("Tolerance: " + drink.barrel.tolerance);
-$("#barrel-da").text("Drinking Ability: " + drink.barrel.drinkingAbility);
+var charPick = false;
+var beerPickOne = false;
+var victorTwo = false;
+var victorThree = false;
 
-$("#ipa-abv").text("ABV: " + bev.ipa.abv);
-$("#ipa-oun").text("Ounces: " + bev.ipa.ounces);
-$("#stout-abv").text("ABV: " + bev.stout.abv);
-$("#stout-oun").text("Ounces: " + bev.stout.ounces);
-$("#pils-abv").text("ABV: " + bev.pils.abv);
-$("#pils-oun").text("Ounces: " + bev.pils.ounces);
+//Set HTML elements/attributes to object elements
+
+$("#malt-tol").text(drink.malt.tolerance);
+$("#malt").attr("tolerance", drink.malt.tolerance);
+$("#malt-da").text(drink.malt.drinkingAbility);
+$("#malt").attr("da", drink.malt.drinkingAbility);
+
+$("#gose-tol").text(drink.gose.tolerance);
+$("#gose").attr("tolerance", drink.gose.tolerance);
+$("#gose-da").text(drink.gose.drinkingAbility);
+$("#gose").attr("da", drink.gose.drinkingAbility);
+
+$("#barrel-tol").text(drink.barrel.tolerance);
+$("#barrel").attr("tolerance", drink.barrel.tolerance);
+$("#barrel-da").text(drink.barrel.drinkingAbility);
+$("#barrel").attr("da", drink.barrel.drinkingAbility);
+
+$("#ipa-abv").text(bev.ipa.abv);
+$("#ipa").attr("abv", bev.ipa.abv);
+$("#ipa-oun").text(bev.ipa.ounces);
+$("#ipa").attr("oun", bev.ipa.ounces);
+
+$("#stout-abv").text(bev.stout.abv);
+$("#stout").attr("abv", bev.stout.abv);
+$("#stout-oun").text(bev.stout.ounces);
+$("#stout").attr("oun", bev.stout.ounces);
+
+$("#pils-abv").text(bev.pils.abv);
+$("#pils").attr("abv", bev.pils.abv);
+$("#pils-oun").text(bev.pils.ounces);
+$("#pils").attr("oun", bev.pils.ounces);
 
 
 function drinkAction() {
@@ -64,24 +93,33 @@ function drinkAction() {
 
 $(".player").on("click", function() {
     chosenDrinker = this;
+    attackTol = $(this).attr("tolerance");
+    attackDA = $(this).attr("da");
+    attackDAInit = $(this).attr("da");
     $("#pick-drinker").empty();
     $("#drinker").append(chosenDrinker);
     $("#beers-on-tap").css("display", "block");
-    playPick = true;
+    charPick = true;
 });
 
 $(".on-tap").on("click", function() {
-    if (playPick) {
+    if (charPick) {
         currBeer = this;
+        defendABV = $(this).attr("abv");
+        defendOZ = $(this).attr("oun");
         $("#bar").append(currBeer);
-        beerPick = true;
+        beerPickOne = true;
     }
 
 })
 
 $("#drink-btn").on("click", function () {
-    if (beerPick === true && playPick === true) {
-
+    if (beerPickOne === true && charPick === true) {
+        attackTol = attackTol - defendABV;
+        defendOZ = defendOZ - attackDA;
+        //attackDA = attackDA + attackDAInit;
+        console.log(attackTol);
+        console.log(defendOZ);
     }
 })
 
